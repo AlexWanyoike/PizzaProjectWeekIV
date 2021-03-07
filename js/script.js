f7b66be1cb502542714b5d2e80ef21358bb9e663
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-07 11:22:50 +0300

    Delivery Changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const form1 = document.getElementById('Form1');
+const form2 = document.getElementById('Form2');
 const pizzasize = document.getElementById('pizzasize');
 const pizzacrust = document.getElementById('pizzacrust');
 const pizzatoppings = document.getElementById('form');
 const pizzanumber = document.getElementById('pizzanumber');
 
+let Next1 = document.getElementById("Next1");
+let Back2 = document.getElementById("Back2");
+
+Next1.onclick=function(){
+  Form1.style.left="-450px";
+  Form2.style.left="40px";
+}
   
  form.addEventListener('checkout'),(e)=>{
    e.preventDefault();
@@ -33,10 +41,4 @@ const pizzanumber = document.getElementById('pizzanumber');
   let pizza2= new pizzas("periperimedium",800);
   let pizza3= new pizzas("periperismall",600);
 
-  <div class="form-control">
-    <label for="">Delivery services</label>
-        <div>
-        <button type="button" class="btn btn-primary btn-sm">Yes</button>
-        <button type="button" class="btn btn-secondary btn-sm">Checkout</button>
-        </div>  
-    </div>
+  
