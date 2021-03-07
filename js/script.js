cf2b24fdd25fe9dcc0af34508467783317eae6aa
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-07 15:32:40 +0300

    Totals

--- js/script.js
+++ js/script.js
@@ -1,32 +1,65 @@
-const form1 = document.getElementById('Form1');
-const form2 = document.getElementById('Form2');
-const pizzasize = document.getElementById('pizzasize');
-const pizzacrust = document.getElementById('pizzacrust');
-const pizzatoppings = document.getElementById('form');
+let form1 = document.getElementById('Form1');
+let form2 = document.getElementById('Form2');
+let pizzasize = document.getElementById('pizzasize')
+let pizzacrust = document.getElementById('pizzacrust');
+let pizzatoppings = document.getElementById('pizzatoppings');
 let pizzanumber = document.getElementById('pizzanumber');
+let totalTag = document.getElementById('output');
+let addPizza = document.getElementById('addpizza');
 
-let Next1 = document.getElementById("Next1");
-let Back2 = document.getElementById("Back2");
-let addpizza = document.getElementById('addpizza');
-  
- function checkInputs() {
-   const pizzasizeValue = pizzasize.value.trim();
-   const pizzacrustValue = pizzacrust.value.trim();
-   const pizzatoppingsValue = pizzatoppings.value.trim();
-   const pizzanumber = pizzasize.number.trim();
- }
- function pizzas(name,amount){
-  this.name=name;
-  this.amount=amount;
+function getSize(){
+  let size = document.getElementById('pizzasize').value
+  if(size==='small'){
+    return(600);
+  }else if(size==='medium'){
+    return(800);
+  }else if(size==='large'){
+    return(1000)
+  }
 }
 
-let pizza1= new pizzas("periperilarge",1000);
-let pizza2= new pizzas("periperimedium",800);
-let pizza3= new pizzas("periperismall",600);
+function getToppings(){
+  let toppings = document.getElementById('pizzatoppings').value
+  if(toppings==='none'){
+    return(0);
+  }else if(toppings==='mushrooms'){
+    return(30);
+  }else if(toppings==='tikkasauce'){
+    return(40);
+  }else if(toppings==='chilli'){
+    return(50)
+  }
+}
 
+function total(){
+  var sum = (getSize()+getToppings());
+  return sum;
+}
+
+
+
+
+
+function getName(event){
+  event.preventDefault()
+  let pizzasize = document.getElementById('pizzasize').value
+  let pizzacrust = document.getElementById('pizzacrust').value
+  let pizzatoppings = document.getElementById('pizzatoppings').value
+  let pizzanumber = document.getElementById('pizzanumber').value
   
-  
-  
-  
+  let summaryTag = document.getElementById('summary')
+  let message = ` You have order a ${pizzasize} ${pizzacrust} ${pizzatoppings}`
+  summaryTag.innerHTML = message;
+  let totalTag = document.getElementById('output');
+  let outputMessage = `Your Bill is ${total()}`
+  totalTag.innerHTML = outputMessage;
+}
+
+addPizza.onclick = function(){
+  sumaryTag.innerHTML = message;
+  form1.reset
+}
+
+
   
  
