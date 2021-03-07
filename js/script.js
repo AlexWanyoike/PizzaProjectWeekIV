48b53dd0ebde8003c9317b1d5f3fae97e3c05f7a
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-07 12:50:27 +0300

    README Added

--- js/script.js
+++ js/script.js
@@ -15,19 +15,18 @@ let addpizza = document.getElementById('addpizza');
    const pizzatoppingsValue = pizzatoppings.value.trim();
    const pizzanumber = pizzasize.number.trim();
  }
-  
-  
-  
-  
-  
-  
-  function pizzas(name,amount){
-    this.name=name;
-    this.amount=amount;
-  }
+ function pizzas(name,amount){
+  this.name=name;
+  this.amount=amount;
+}
 
-  let pizza1= new pizzas("periperilarge",1000);
-  let pizza2= new pizzas("periperimedium",800);
-  let pizza3= new pizzas("periperismall",600);
+let pizza1= new pizzas("periperilarge",1000);
+let pizza2= new pizzas("periperimedium",800);
+let pizza3= new pizzas("periperismall",600);
 
   
+  
+  
+  
+  
+ 
