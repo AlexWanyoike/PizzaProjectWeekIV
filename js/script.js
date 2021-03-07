6b5ccea255ac1b3312290c1ecb518be4daf4c486
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-03-07 03:01:18 +0300

    Images tracked

---
+++ js/script.js
@@ -0,0 +1,14 @@
+  function pizzas(name,amount){
+    this.name=name;
+    this.amount=amount;
+  }
+
+  let pizza1= new pizzas("periperilarge",1000);
+  let pizza2= new pizzas("periperimedium",800);
+  let pizza3= new pizzas("periperismall",600);
+
+  $(document).ready(function(){
+    
+
+
+  });
